bea5c4e0567acfb9a7017e81439872896f2826d3
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-10-23 21:37:12 +0300

    refactored edit screen

--- lib/presentation/screens/edit_screen/view/edit_screen.dart
+++ lib/presentation/screens/edit_screen/view/edit_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:job_keeper/presentation/common/job_form_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:job_keeper/domain/entities/job_entity.dart';
+import 'package:job_keeper/presentation/common/job_form_widget.dart';
 import 'package:job_keeper/presentation/screens/edit_screen/view_model/edit_view_model.dart';
 import 'package:job_keeper/presentation/screens/edit_screen/view_model/edit_view_model_state.dart';
 
@@ -19,16 +19,19 @@ class _EditScreenState extends State<EditScreen> {
   late final EditViewModel _viewModel;
   late EditViewModelState _state;
   late JobEntity? _job;
-  late TextEditingController _companyNameController;
-  late TextEditingController _companyWebsiteController;
-  late TextEditingController _vacancyLinkController;
-  late TextEditingController _mainCommentController;
-  late TextEditingController _hrContactController;
-  late TextEditingController _locationController;
-  late TextEditingController _positionController;
-  late TextEditingController _vacancySalaryController;
-  late TextEditingController _preferredSalaryController;
-  late TextEditingController _extraCommentController;
+  final TextEditingController _companyNameController = TextEditingController();
+  final TextEditingController _companyWebsiteController =
+      TextEditingController();
+  final TextEditingController _vacancyLinkController = TextEditingController();
+  final TextEditingController _mainCommentController = TextEditingController();
+  final TextEditingController _hrContactController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _positionController = TextEditingController();
+  final TextEditingController _vacancySalaryController =
+      TextEditingController();
+  final TextEditingController _preferredSalaryController =
+      TextEditingController();
+  final TextEditingController _extraCommentController = TextEditingController();
 
   @override
   void didChangeDependencies() {
@@ -41,36 +44,16 @@ class _EditScreenState extends State<EditScreen> {
     _state = _viewModel.state;
     _job = _state.job;
     if (_job != null) {
-      _companyNameController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.companyName),
-      );
-      _companyWebsiteController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.companyLink ?? ''),
-      );
-      _vacancyLinkController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.vacancyLink ?? ''),
-      );
-      _mainCommentController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.mainComment),
-      );
-      _hrContactController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.contact ?? ''),
-      );
-      _locationController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.companyLocation ?? ''),
-      );
-      _positionController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.position ?? ''),
-      );
-      _vacancySalaryController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.vacancySalary ?? ''),
-      );
-      _preferredSalaryController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.preferredSalary ?? ''),
-      );
-      _extraCommentController = TextEditingController.fromValue(
-        TextEditingValue(text: _job!.extraComment ?? ''),
-      );
+      _companyNameController.text = _job!.companyName;
+      _companyWebsiteController.text = _job!.companyLink ?? '';
+      _vacancyLinkController.text = _job!.vacancyLink ?? '';
+      _mainCommentController.text = _job!.mainComment;
+      _hrContactController.text = _job!.contact ?? '';
+      _locationController.text = _job!.companyLocation ?? '';
+      _positionController.text = _job!.position ?? '';
+      _vacancySalaryController.text = _job!.vacancySalary ?? '';
+      _preferredSalaryController.text = _job!.preferredSalary ?? '';
+      _extraCommentController.text = _job!.extraComment ?? '';
     }
     super.didChangeDependencies();
   }
@@ -121,7 +104,8 @@ class _EditScreenState extends State<EditScreen> {
                     positionController: _positionController,
                     vacancySalaryController: _vacancySalaryController,
                     preferredSalaryController: _preferredSalaryController,
-                    extraCommentController: _extraCommentController),
+                    extraCommentController: _extraCommentController,
+                  ),
           ),
         ),
       ),
